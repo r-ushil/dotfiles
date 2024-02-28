2d5f3531bf3a5ff9e18c3929a1017861c106d2d5
Rushil Patel <rushil.patel791@gmail.com>
2024-02-28 23:38:32 +0000

    feat: switched to wofi

---
+++ sway/exit-menu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define options
+options="suspend\nlock\nshutdown\nreboot\nexit sway"
+
+# Show the menu
+selected=$(echo -e $options | wofi --show dmenu --width 300 --height 300 --lines 6)
+
+# Execute the selected option
+case $selected in
+    "suspend")
+        swaylock -c 050505 && systemctl suspend
+        ;;
+    "lock")
+        swaylock -c 050505
+        ;;
+    "shutdown")
+        systemctl -i poweroff
+        ;;
+    "reboot")
+        systemctl -i reboot
+        ;;
+    "exit sway")
+        swaymsg exit
+        ;;
+esac
+
